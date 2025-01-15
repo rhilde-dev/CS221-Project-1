2104503976834728c6b0bcd77fa9866bd3622fd0
rhilde-dev <ryleehilde@u.boisestate.edu>
2025-01-15 09:59:14 -0700

    Created GridMonitor class and added all classes for interface; Starting work on classes

---
+++ GridMonitor.java
@@ -0,0 +1,46 @@
+public class GridMonitor implements GridMonitorInterface {
+    public static void main(String[] args) {
+        
+    }
+
+    public GridMonitor(){
+
+    }
+
+    @Override
+    public double[][] getBaseGrid() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public double[][] getSurroundingSumGrid() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public double[][] getSurroundingAvgGrid() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public double[][] getDeltaGrid() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean[][] getDangerGrid() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return super.toString();
+    }
+
+}
