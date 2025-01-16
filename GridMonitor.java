13586093c1387a35fba12ac7958d61ad338fcca2
rhilde-dev <ryleehilde@u.boisestate.edu>
2025-01-16 09:57:11 -0700

    fixed exception error; need to finish adding file num to array

--- GridMonitor.java
+++ GridMonitor.java
@@ -2,51 +2,106 @@ import java.util.Scanner;
 import java.io.*;
 
 public class GridMonitor implements GridMonitorInterface {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         GridMonitor grid = new GridMonitor("sample.txt");
-        
+
     }
 
+    /**
+     * 
+     * 
+     * @param filename
+     * @throws FileNotFoundException
+     */
     public GridMonitor(String filename) throws FileNotFoundException{
-        
+
+        int row = 0;
+
+        //scan file
         File file = new File(filename);
         Scanner fileScan = new Scanner(file);
-        System.out.println(fileScan.nextLine());
+        
+        //scan first line for array dimensions
+        Scanner lineScan = new Scanner(fileScan.nextLine());
+        lineScan.useDelimiter(" ");
 
+        //intialize new array with dimensions
+        int[][] array = new int[Integer.parseInt(lineScan.next())][Integer.parseInt(lineScan.next())]; 
+        lineScan.close();
+        
+        //initialize spaces with next lines
+        while (fileScan.hasNextLine()){
+            Scanner arrayScan = new Scanner(fileScan.nextLine());
+            arrayScan.useDelimiter(" ");
+
+            for (int i = 0; i < array[row].length; i++){
+                //add each number to the row
+
+
+            }
+
+            row++;
+
+        }
+        
         fileScan.close();
 
     }
 
+    /**
+     * 
+     *  
+     */
     @Override
     public double[][] getBaseGrid() {
         // TODO Auto-generated method stub
         return null;
     }
 
+    /**
+     * 
+     * 
+     */
     @Override
     public double[][] getSurroundingSumGrid() {
         // TODO Auto-generated method stub
         return null;
     }
 
+    /**
+     * 
+     * 
+     */
     @Override
     public double[][] getSurroundingAvgGrid() {
         // TODO Auto-generated method stub
         return null;
     }
 
+    /**
+     * 
+     * 
+     */
     @Override
     public double[][] getDeltaGrid() {
         // TODO Auto-generated method stub
         return null;
     }
 
+    /**
+     * 
+     * 
+     */
     @Override
     public boolean[][] getDangerGrid() {
         // TODO Auto-generated method stub
         return null;
     }
 
+    /**
+     * 
+     * 
+     */
     @Override
     public String toString() {
         // TODO Auto-generated method stub
